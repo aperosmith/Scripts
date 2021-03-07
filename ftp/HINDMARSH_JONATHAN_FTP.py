f9f6df2d9bdfc658cb5c47e33b97b4bf7cfcb458
Ventouz <potoroo725@gmail.com>
2021-03-07 17:58:20 +0100

    Update HINDMARSH_JONATHAN_FTP.py

---
+++ ftp/HINDMARSH_JONATHAN_FTP.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+#
+import os
+import time
+import sys
+import socket
+import struct
+
+FTP_IP = "127.0.0.1"
+FTP_PORT = 2121
+BUFFER_SIZE = 1024
+
+print("STARTING FTP SERVER")
+
+
+
+
+
+
+
+
+
+def main():
+    try:
+        ftpd.serve_forev
+
+if __name__ == "__main__":
+    # FTP SERVER SETUP
+    IP = '127.0.0.1'
+    PORT = 21
+    server = ServerFTP(IP, PORT)
+    print('FTP Server - {}:{} \n'.format( IP, PORT))
