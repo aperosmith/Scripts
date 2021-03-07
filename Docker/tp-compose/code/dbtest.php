f1883dca7e55c87eea1b6e6fb3cf91b4f86447df
Ventouz <potoroo725@gmail.com>
2021-03-07 17:25:14 +0100

    devops

---
+++ Docker/tp-compose/code/dbtest.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "mariadb";
+$username = "root";
+$password = "root";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+ die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
